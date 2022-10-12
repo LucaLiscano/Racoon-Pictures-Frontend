0f64652d6c51c704d45bad85f59d3bcc78469166
Your Name <you@example.com>
2022-10-12 16:52:23 -0300

    adelanto de la seccion artes

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+function scrollL() {
+  let left = document.querySelector(".scroll-images");
+  left.scrollBy(555, 0);
+}
+
+function scrollR() {
+  let right = document.querySelector(".scroll-images");
+  right.scrollBy(-555, 0);
+}
