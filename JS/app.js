7c80e0c586ba0b005d693cd40e1f73c9ef683022
Luca Liscano <luca.liscano@anima.edu.uy>
2022-10-17 22:11:47 -0300

    arreglos

--- JS/app.js
+++
@@ -1,9 +0,0 @@
-function scrollL() {
-  let left = document.querySelector(".scroll-images");
-  left.scrollBy(555, 0);
-}
-
-function scrollR() {
-  let right = document.querySelector(".scroll-images");
-  right.scrollBy(-555, 0);
-}
